c46c4e5cbc3d57aff6e1c1918805f14bab2c5a37
tejasvishelar <39942692+tejasvishelar@users.noreply.github.com>
2024-07-14 16:15:10 +0530

    Update userdata1.sh

--- day-24/userdata1.sh
+++ day-24/userdata1.sh
@@ -30,9 +30,9 @@ cat <<EOF > /var/www/html/index.html
   </style>
 </head>
 <body>
-  <h1>Terraform Project Server 1</h1>
+  <h1>Terraform Project Server 2</h1>
   <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
-  <p>Welcome to CloudChamp's Channel</p>
+  <p>Welcome to Krisha's Channel</p>
   
 </body>
 </html>
